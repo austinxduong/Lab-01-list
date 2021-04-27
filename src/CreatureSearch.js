442eeddb06b218dcff7ead0d9bbbc725e7d7981e
austinxduong <axduong2@gmail.com>
2021-04-27 16:07:44 -0700

    designed style form

--- src/CreatureSearch.js
+++ src/CreatureSearch.js
@@ -6,6 +6,16 @@ export default class CatSearch extends Component {
   render() {
     return (
       <form className="CreatureSearch">
+        <input/>
+
+        <select>
+          <option value="">sort...</option>
+          <option value="name"> by name</option>
+          <option value="horns">by lives</option>
+        </select>
+
+
+
         <button>🔎</button>
       </form>
     );
